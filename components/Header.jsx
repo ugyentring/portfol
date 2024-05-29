aca4b368e99dbbe9a3e896ca36f95a90cd17d962
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-30 04:44:28 +0600

    donw with navbar design

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,33 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
 
+//component import
+import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header>
-      header
-    </header>
-  )
-}
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* My logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Ugyen<span className="text-accent">.</span>
+          </h1>
+        </Link>
 
-export default Header
+        {/* Desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Contact Me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">mdndj</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
