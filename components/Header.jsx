71b764a16f27e1c35dda4c6f2a0b35d21129163f
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-30 12:56:17 +0600

    mobile nav done

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 
 //component import
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -24,7 +25,9 @@ const Header = () => {
         </div>
 
         {/* mobile nav */}
-        <div className="xl:hidden">mdndj</div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
