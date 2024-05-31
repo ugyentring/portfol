688fe4eacc02e1aeddf0b162281373bd14659a7b
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-31 17:29:10 +0600

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,45 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
+//component import
+import Social from "@/components/Social";
+
+const Home = () => {
   return (
-    <main>
-      homepage
-    </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I am <br />{" "}
+              <span className="text-accent">Ugyen Tshering</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Fond of crafting elegant digital experiences with MERN stack and
+              Web 3.0 Technologies like Solidity, Hardhat, EtherJS and Alchemy
+            </p>
+
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+
+              <div className="mb-8 xl:mb-0">
+                <Social />
+              </div>
+            </div>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
