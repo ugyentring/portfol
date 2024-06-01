1e44b824c3986433c2d4e26145a5e809128fc269
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-06-01 13:56:04 +0600

    image and circle in homepage

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -19,21 +19,49 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/assets/pro.png"
+            src="/assets/face.png"
             priority
             quality={100}
             fill
-            className="object-contain"
+            className="object-contain rounded-full grayscale"
             alt="profile"
           />
         </motion.div>
 
         {/*circle*/}
-          <motion.svg></motion.svg>
-
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: [
+                "15, 120, 25, 25",
+                "16, 25, 92, 72",
+                "4, 250, 22, 22",
+              ],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
