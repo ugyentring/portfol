ed50a7eaaf707059453e39b9062d0b286ba5addc
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-06-01 21:53:51 +0600

    changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
         <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
-            <Button className="text-black">Contact Me</Button>
+            <Button className="text-white rounded-md">Contact Me</Button>
           </Link>
         </div>
 
