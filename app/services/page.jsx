4cf00e80249d726b8711f236d41bb4148f1be5bd
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-06-01 18:10:57 +0600

    service page done!

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -54,17 +54,22 @@ const Services = () => {
               >
                 {/* top */}
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
                     {service.num}
                   </div>
-                  <Link href={service.href}>
-                    <BsArrowDownRight />
+                  <Link
+                    href={service.href}
+                    className="w-[70px] h-[70px] rounded-full bg-black group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
                 {/* heading */}
-                <h2>{service.title}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-black group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
                 {/* description */}
-                <p>{service.description}</p>
+                <p className="text-black/60">{service.description}</p>
                 {/* border */}
                 <div className="border-b border-black/20 w-full"></div>
               </div>
