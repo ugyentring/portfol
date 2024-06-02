36ba221f3887a167a8d6d66eccbb63828f2b4bd9
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-06-02 10:13:29 +0600

    donw with resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -301,8 +301,29 @@ const Resume = () => {
             </TabsContent>
 
             {/*about me*/}
-            <TabsContent value="about" className="w-full">
-              about
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col group-[30px]:">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-black/80 mx-auto xl:mx-0 mb-4">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-black/60">{item.fieldName}</span>
+                        <span className="text-xl">{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
